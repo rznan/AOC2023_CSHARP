66835dd75475c763bebe4e3097a3a70400aa7bfd
Renan <109163037+rznan@users.noreply.github.com>
2023-12-12 20:19:10 -0300

    Dia 3 parte 1 completa, +corrigindo alguns erros

--- Puzzles/Day03.cs
+++ Puzzles/Day03.cs
@@ -1,53 +1,89 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Puzzles
 {
     public class Day03
     {
-        public static int Part1Solution(string[] input)
+        public static long Part1Solution(string[] input)
         {
-            int partNumberSum = 0;
+            long partNumberSum = 0;
             int lineAmmount = input.Length;
             List<int> digitsBuffer = [];
 
             for(int lineNumber = 0; lineNumber < lineAmmount; lineNumber++)
             {
                 int lineLength = input[lineNumber].Length;
+                int numberStartPositon = 0;
 
-                for(int cursorPosition = 0; cursorPosition < lineLength; lineNumber++)
+                for(int cursorPosition = 0; cursorPosition < lineLength; cursorPosition++)
                 {
                     // Finds the digits in a line and store in digitsBuffer. After a
                     // sequence of 1 or more number ends, or its the last char in a line,
                     // it checks if its a part number, if it is then it adds to the sum
                     if(int.TryParse(input[lineNumber][cursorPosition].ToString(), out int valor))
                     {
+                        if(digitsBuffer.Count == 0)
+                            numberStartPositon = cursorPosition;
                         digitsBuffer.Add(valor);
-                        if(!(cursorPosition == lineLength -1)) continue;
+                        if(cursorPosition < lineLength -1) continue;
                     }
 
                     // case found a non-digit char or last position in line
-                    if(digitsBuffer.Count > 0)
+                    if(digitsBuffer.Count != 0)
                     {
-                        if(IsPartNumber(lineNumber:lineNumber, numberEndPositon:cursorPosition-1, numberLenght:digitsBuffer.Count, input:input))
+                        if(IsPartNumber(numberLine:lineNumber, numberStartPositon:numberStartPositon, numberLenght:digitsBuffer.Count, input:input))
                             partNumberSum += AssembleNumber(digitsBuffer);
                         digitsBuffer.Clear();
+                        //Console.WriteLine(partNumberSum);
                     }
                 }
             }
-            throw new NotImplementedException();
+
+            return partNumberSum;
         }
 
-        private static bool IsPartNumber(int lineNumber, int numberEndPositon, int numberLenght, string[] input)
+        private static bool IsPartNumber(int numberLine, int numberStartPositon, int numberLenght, string[] input)
         {
-            throw new NotImplementedException();
+            // percorre os arredores de um número em busca de algum simbolo
+            // caso encontre um simbolo então o número inserido é valido
+            
+            // percorre desde uma linha antes até uma linha depois
+            for(int currentLine = numberLine-1; currentLine <= numberLine+1; currentLine++)
+            {
+                if(currentLine >= 0 && currentLine < input.Length)
+                {
+                    // percorre desde um caráctere antes até um caractére depois
+                    for(int cursorPosition = numberStartPositon - 1; cursorPosition <= numberStartPositon + numberLenght; cursorPosition++)
+                    {
+                        if(cursorPosition >= 0 && cursorPosition < input[currentLine].Length)
+                        {
+                            // checa se o caractére atual é um simbolo válido
+                            if(input[currentLine][cursorPosition] != '.' && !int.TryParse(input[currentLine][cursorPosition].ToString(), out int _))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
         }
 
-        private static int AssembleNumber(List<int> digits)
+        public static int AssembleNumber(List<int> digits)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            for(int cont = 0; cont < digits.Count; cont++)
+            {
+                result += digits[cont];
+                result = cont < digits.Count - 1 ? result * 10 : result;
+            }
+
+            return result;
         }
 
         public static int Part2Solution(string[] input)
--- Puzzles/Program.cs
+++ Puzzles/Program.cs
@@ -12,37 +12,44 @@ while (true)
     );
     if(int.TryParse(Console.ReadLine(), out int opcao))
     {
-        string[] input;
-        switch(opcao)
+        try
         {
-            case 1:
-                input = File.ReadAllLines(".\\Day01.csv");
-                Console.WriteLine("Dia 1 - Trebutchet");
-                Console.WriteLine($"\tParte 1: \t{Day01.Part1Solution(input)}");
-                Console.WriteLine($"\tParte 2: \t{Day01.Part2Solution(input)}");
-                break;
+            string[] input;
+            switch(opcao)
+            {
+                case 1:
+                    input = File.ReadAllLines(".//Day01.csv");
+                    Console.WriteLine("Dia 1 - Trebutchet");
+                    Console.WriteLine($"\tParte 1: \t{Day01.Part1Solution(input)}");
+                    Console.WriteLine($"\tParte 2: \t{Day01.Part2Solution(input)}");
+                    break;
 
-            case 2:
-                input = File.ReadAllLines(".\\Day02.txt");
-                Console.WriteLine("Day 2: Cube Conundrum");
-                Console.WriteLine($"\tParte 1: \t{Day02.Part1Solution(input)}");
-                Console.WriteLine($"\tParte 2: \t{Day02.Part2Solution(input)}");
-                break;
+                case 2:
+                    input = File.ReadAllLines(".//Day02.txt");
+                    Console.WriteLine("Day 2: Cube Conundrum");
+                    Console.WriteLine($"\tParte 1: \t{Day02.Part1Solution(input)}");
+                    Console.WriteLine($"\tParte 2: \t{Day02.Part2Solution(input)}");
+                    break;
 
-            case 3:
-                input = File.ReadAllLines(".\\Day03.txt");
-                Console.WriteLine("Day 3: Gear Ratios");
-                Console.WriteLine($"\tParte 1: \t{Day03.Part1Solution(input)}");
-                Console.WriteLine($"\tParte 2: \t{Day03.Part2Solution(input)}");
-                break;
+                case 3:
+                    input = File.ReadAllLines(".//Day03.txt");
+                    Console.WriteLine("Day 3: Gear Ratios");
+                    Console.WriteLine($"\tParte 1: \t{Day03.Part1Solution(input)}");
+                    //Console.WriteLine($"\tParte 2: \t{Day03.Part2Solution(input)}");
+                    break;
 
-            case 99: 
-                System.Environment.Exit(0);
-                break;
+                case 99: 
+                    System.Environment.Exit(0);
+                    break;
 
-            default:
-                Console.WriteLine("Não foi possível encontrar o dia");
-                break;
+                default:
+                    Console.WriteLine("Não foi possível encontrar o dia");
+                    break;
+            }
+        } 
+        catch(FileNotFoundException)
+        {
+            Console.WriteLine("Não foi possível encontrar o input desse dia");
         }
     }
     else
