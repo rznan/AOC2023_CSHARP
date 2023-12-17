2a06cd211420f181f64c6693901953a411ec6278
Renan <109163037+rznan@users.noreply.github.com>
2023-12-17 12:17:34 -0300

    Parte 2 do segundo dia completa

--- Puzzles/Day03.cs
+++ Puzzles/Day03.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-
 namespace Puzzles
 {
     public class Day03
@@ -12,7 +6,7 @@ namespace Puzzles
         {
             long partNumberSum = 0;
             int lineAmmount = input.Length;
-            List<int> digitsBuffer = [];
+            Queue<int> digitsBuffer = [];
 
             for(int lineNumber = 0; lineNumber < lineAmmount; lineNumber++)
             {
@@ -28,8 +22,8 @@ namespace Puzzles
                     {
                         if(digitsBuffer.Count == 0)
                             numberStartPositon = cursorPosition;
-                        digitsBuffer.Add(valor);
-                        if(cursorPosition < lineLength -1) continue;
+                        digitsBuffer.Enqueue(valor);
+                        if(cursorPosition < lineLength -1) continue; //check if is last char in line
                     }
 
                     // case found a non-digit char or last position in line
@@ -38,7 +32,6 @@ namespace Puzzles
                         if(IsPartNumber(numberLine:lineNumber, numberStartPositon:numberStartPositon, numberLenght:digitsBuffer.Count, input:input))
                             partNumberSum += AssembleNumber(digitsBuffer);
                         digitsBuffer.Clear();
-                        //Console.WriteLine(partNumberSum);
                     }
                 }
             }
@@ -47,26 +40,117 @@ namespace Puzzles
 
         public static int Part2Solution(string[] input)
         {
+            // find the gear ratio of the engine. Searches for any '*' and checks
+            // the numbers arround it to get the gear ratio of the engine
+            int gearRatio = 0;
+            int lineAmmount = input.Length;
 
-            throw new NotImplementedException();
+            for(int lineNumber = 0; lineNumber < lineAmmount; lineNumber++)
+            {
+                int lineLength = input[lineNumber].Length;
+                for(int cursorPosition = 0; cursorPosition < lineLength; cursorPosition++)
+                { 
+                    if(input[lineNumber][cursorPosition].Equals('*'))
+                    {
+                        gearRatio += GetGearRatio(yPosition:lineNumber, xPosition:cursorPosition, input:input);
+                    }
+                }
+            }
+            return gearRatio;
+        }
+
+        private static int GetGearRatio(int yPosition, int xPosition, string[] input)
+        {
+            // uses the coordinates of a '*' to check for a gear ratio.
+            // to be a gear ratio there must be only 2 numbers around 
+            // the '*'. Their multiplication is the gear ratio
+            Queue<int> numberBuilder = new();
+            int numberCount = 0;
+            int gearRatio = 1;
+
+            for(int line = yPosition-1; line <= yPosition+1; line++)
+            {
+                for(int cursorPosition = xPosition-1; cursorPosition <= xPosition+1; cursorPosition++)
+                {
+                    try
+                    {
+                        // Check it current char is a number. If true checks the next char. 
+                        if(int.TryParse(input[line][cursorPosition].ToString(), out int digit))
+                        {
+                            cursorPosition = GetNumberDigits(numberBuilder, line, cursorPosition, input); // sets the cursor to the end of the number
+                            if(cursorPosition <= xPosition && cursorPosition < input[line].Length-1) continue; // check if is last char in the sorroundings
+                        }
+                    }
+                    catch(IndexOutOfRangeException){} 
+                    finally
+                    {
+                        // builds a number and adds it to the gear ratio
+                        if(numberBuilder.Count > 0)
+                        {
+                            gearRatio *= AssembleNumber(numberBuilder);
+                            numberCount++;
+                            numberBuilder.Clear();
+                        }
+                    }
+                }
+            }
+            return numberCount == 2 ? gearRatio : 0;
+        }
+
+        private static int GetNumberDigits(Queue<int> numberBuilder, int yPosition, int xPosition, string[] input)
+        {   
+            // fill the numberBuilder with the number specified by the given
+            // coordinates. The coordinate can be any digit of the number.
+            // returns the position of the number's last digit
+            int lastDigitPosition = xPosition;
+            int reverseIterator = xPosition;
+
+            Stack<int> digitsBefore = new();
+
+            // adds any digit before the provided coordinates
+            try
+            {
+                while(int.TryParse(input[yPosition][reverseIterator].ToString(), out int digit))
+                {
+                    digitsBefore.Push(digit);
+                    reverseIterator--;
+                }
+            }
+            catch(IndexOutOfRangeException){}
+            finally
+            {
+                while(digitsBefore.Count > 0)
+                {
+                    numberBuilder.Enqueue(digitsBefore.Pop());
+                }
+            }
+
+            // ads any digit after the provided coordinates
+            try
+            {
+                while(int.TryParse(input[yPosition][lastDigitPosition+1].ToString(), out int digit))
+                {
+                    numberBuilder.Enqueue(digit);
+                    lastDigitPosition++;
+                }
+            }
+            catch(IndexOutOfRangeException){}
+
+            return lastDigitPosition;
         }
 
         private static bool IsPartNumber(int numberLine, int numberStartPositon, int numberLenght, string[] input)
         {
-            // percorre os arredores de um número em busca de algum simbolo
-            // caso encontre um simbolo então o número inserido é valido
-            
-            // percorre desde uma linha antes até uma linha depois
+            // iterates throgh the number's surrounding characters
             for(int currentLine = numberLine-1; currentLine <= numberLine+1; currentLine++)
             {
                 if(currentLine >= 0 && currentLine < input.Length)
                 {
-                    // percorre desde um caráctere antes até um caractére depois
                     for(int cursorPosition = numberStartPositon - 1; cursorPosition <= numberStartPositon + numberLenght; cursorPosition++)
                     {
                         try
                         {
-                            // checa se o caractére atual é um simbolo válido
+                            // checks if current char is valid
                             if(input[currentLine][cursorPosition] != '.' && !int.TryParse(input[currentLine][cursorPosition].ToString(), out int _))
                             {
                                 return true;
@@ -80,16 +164,15 @@ namespace Puzzles
             return false;
         }
 
-        public static int AssembleNumber(List<int> digits)
+        public static int AssembleNumber(Queue<int> digits)
         {
             int result = 0;
-            for(int cont = 0; cont < digits.Count; cont++)
+            while(digits.Count > 0)
             {
-                result += digits[cont];
-                result = cont < digits.Count - 1 ? result * 10 : result;
+                result = (result * 10) + digits.Dequeue();
             }
-
             return result;
         }
+
     }
 }
